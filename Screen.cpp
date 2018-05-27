c2a0ccc6562958a6df3b12d4747e3c1291f206f2
Conner Tenn <connertenn@gmail.com>
2018-05-27 03:44:27 -0400

    Fixed boarder displays on tiles

--- Screen.cpp
+++ Screen.cpp
@@ -25,15 +25,38 @@ std::string GetFillLine(WINDOW *win, char chr)
 
 void Border(WINDOW *win, int x1, int y1, int x2, int y2)
 {
-	wmove(win, y1, x1+1); whline(win, ACS_HLINE, x2-x1);
-	wmove(win, y2, x1+1); whline(win, ACS_HLINE, x2-x1);
-	wmove(win, y1+1, x1); wvline(win, ACS_VLINE, y2-y1);
-	wmove(win, y1+1, x2); wvline(win, ACS_VLINE, y2-y1);
+	wmove(win, y1, x1+1); whline(win, ACS_HLINE, x2-x1-1);
+	wmove(win, y2, x1+1); whline(win, ACS_HLINE, x2-x1-1);
+	wmove(win, y1+1, x1); wvline(win, ACS_VLINE, y2-y1-1);
+	wmove(win, y1+1, x2); wvline(win, ACS_VLINE, y2-y1-1);
 	
-	wmove(win, y1, x1); waddch(win, ACS_ULCORNER);
-	wmove(win, y1, x2); waddch(win, ACS_URCORNER);
-	wmove(win, y2, x1); waddch(win, ACS_LLCORNER);
-	wmove(win, y2, x2); waddch(win, ACS_LRCORNER);
+	auto replace = [&](int place)->int
+	{
+		const int map[4][11][2] = 
+			{ 
+				//ULCorner
+				{ { ACS_HLINE, ACS_TTEE }, { ACS_VLINE, ACS_LTEE }, { ACS_ULCORNER, ACS_ULCORNER }, { ACS_URCORNER, ACS_TTEE }, { ACS_LLCORNER, ACS_LTEE }, { ACS_LRCORNER, ACS_PLUS }, { ACS_TTEE, ACS_TTEE }, { ACS_BTEE, ACS_PLUS }, { ACS_LTEE, ACS_LTEE }, { ACS_RTEE, ACS_PLUS }, { ACS_PLUS, ACS_PLUS } },
+				//URCorner
+				{ { ACS_HLINE, ACS_TTEE }, { ACS_VLINE, ACS_RTEE }, { ACS_ULCORNER, ACS_TTEE }, { ACS_URCORNER, ACS_URCORNER }, { ACS_LLCORNER, ACS_PLUS }, { ACS_LRCORNER, ACS_RTEE }, { ACS_TTEE, ACS_TTEE }, { ACS_BTEE, ACS_PLUS }, { ACS_LTEE, ACS_PLUS }, { ACS_RTEE, ACS_RTEE }, { ACS_PLUS, ACS_PLUS } },
+				//LLCorner
+				{ { ACS_HLINE, ACS_BTEE }, { ACS_VLINE, ACS_LTEE }, { ACS_ULCORNER, ACS_LTEE }, { ACS_URCORNER, ACS_PLUS }, { ACS_LLCORNER, ACS_LLCORNER }, { ACS_LRCORNER, ACS_BTEE }, { ACS_TTEE, ACS_PLUS }, { ACS_BTEE, ACS_BTEE }, { ACS_LTEE, ACS_LTEE }, { ACS_RTEE, ACS_PLUS }, { ACS_PLUS, ACS_PLUS } },
+				//LRCorner
+				{ { ACS_HLINE, ACS_BTEE }, { ACS_VLINE, ACS_RTEE }, { ACS_ULCORNER, ACS_PLUS }, { ACS_URCORNER, ACS_RTEE }, { ACS_LLCORNER, ACS_BTEE }, { ACS_LRCORNER, ACS_LRCORNER }, { ACS_TTEE, ACS_PLUS }, { ACS_BTEE, ACS_BTEE }, { ACS_LTEE, ACS_PLUS }, { ACS_RTEE, ACS_RTEE }, { ACS_PLUS, ACS_PLUS } },
+			};
+		int curr = winch(win), placeNum = 0;
+		if (place == (int)ACS_ULCORNER) { placeNum = 0; } else if (place == (int)ACS_URCORNER) { placeNum = 1; } else if (place == (int)ACS_LLCORNER) { placeNum = 2; } else if (place == (int)ACS_LRCORNER) { placeNum = 3; }
+		else { return place; }
+		for (int i = 0; i < 11; i++)
+		{
+			if (curr == map[placeNum][i][0]) { return map[placeNum][i][1]; }
+		}
+		return place;
+	};
+	
+	wmove(win, y1, x1); waddch(win, replace(ACS_ULCORNER));
+	wmove(win, y1, x2); waddch(win, replace(ACS_URCORNER));
+	wmove(win, y2, x1); waddch(win, replace(ACS_LLCORNER));
+	wmove(win, y2, x2); waddch(win, replace(ACS_LRCORNER));
 }
 
 Screen::Screen()
@@ -72,8 +95,6 @@ Screen::Screen()
 	TopPad = newpad(1, 1000);
 	Resize();
 	
-	
-	
 	//signal(SIGWINCH, Resize);
 }
 
@@ -105,8 +126,7 @@ void Screen::Resize()
 	if (Pad) { delwin(Pad); }
 	Pad = newpad(MAX(((int)PrinterList.size()/PrinterCols + 1)*(PrinterHeight+1)+1, Height-2), (PrinterWidth+2)*PrinterCols);
 	
-	
-	for (int i = 0; i < (int)PrinterList.size(); i++)
+	for (int i = 0; Pad && i < (int)PrinterList.size(); i++)
 	{
 		if (PrinterList[i].Pad) { delwin(PrinterList[i].Pad); }
 		PrinterList[i].Pad = subpad(Pad, PrinterHeight, PrinterWidth, 0, 0);
@@ -120,7 +140,7 @@ void Screen::Draw()
 	wclear(Pad);
 	
 	wattrset(TopPad, COLOR_PAIR(TOPBAR));
-	waddstr(TopPad, "Name v\tStatus"); FillLine(TopPad, ' ');
+	waddstr(TopPad, "Name\tStatus"); FillLine(TopPad, ' ');
 	//wattrset(stdscr, 0);
 	
 	
@@ -132,12 +152,15 @@ void Screen::Draw()
 		int x = (i % PrinterCols) * (PrinterWidth+1);
 		
 		//if (i == Cursor) { selectedPrinterPos = printerPos; }
-		Border(Pad, x, printerPos-1, x+PrinterWidth+1, printerPos+PrinterHeight);
 		//if (i) { wmove(Pad, printerPos-1, 0); waddch(Pad, ACS_LTEE); wmove(Pad, printerPos-1, PrinterWidth+1); waddch(Pad, ACS_RTEE); }
 		
-		mvderwin(PrinterList[i].Pad, printerPos, x+1); if ((i+1)%PrinterCols == 0) { printerPos+=PrinterHeight+1; } 
+		
+		Border(Pad, x, printerPos-1, x+PrinterWidth+1, printerPos+PrinterHeight);
 		PrinterList[i].Draw(this);
 		//wborder(PrinterList[i].Pad, '1','2','3','4','5','6','7','8');
+		
+		mvderwin(PrinterList[i].Pad, printerPos, x+1);
+		if ((i+1)%PrinterCols == 0) { printerPos+=PrinterHeight+1; } 
 	}
 	
 	/*{
@@ -158,19 +181,24 @@ void Screen::Draw()
 	
 	wattrset(Pad, 0);
 	
-	BottomText += "Width:" + std::to_string(Width) + "  Height:" + std::to_string(Height) + "  PrinterWidth:" + std::to_string(PrinterWidth) + "  PrinterCols:" + std::to_string(PrinterCols);
+	/*move(1,1); 
+	addch(ACS_ULCORNER); addch(ACS_URCORNER); addch(ACS_LLCORNER); addch(ACS_LRCORNER); addch(ACS_PLUS);
+	move(1,1); BottomText+=std::to_string(inch()); BottomText+=":"; BottomText+=std::to_string(ACS_ULCORNER); BottomText+=" ";
+	move(1,2); BottomText+=std::to_string(inch()); BottomText+=":"; BottomText+=std::to_string(ACS_URCORNER); BottomText+=" ";
+	move(1,3); BottomText+=std::to_string(inch()); BottomText+=":"; BottomText+=std::to_string(ACS_LLCORNER); BottomText+=" ";
+	move(1,4); BottomText+=std::to_string(inch()); BottomText+=":"; BottomText+=std::to_string(ACS_LRCORNER); BottomText+=" ";
+	move(1,5); BottomText+=std::to_string(inch()); BottomText+=":"; BottomText+=std::to_string(ACS_PLUS); BottomText+=" ";*/
+	
+	
+	BottomText += "Width:" + std::to_string(Width) + "  Height:" + std::to_string(Height) + "  PrinterWidth:" + std::to_string(PrinterWidth) + "  PrinterCols:" + std::to_string(PrinterCols) + "  Val:" + std::to_string(A_CHARTEXT);
 	mvaddstr(Height-1, 0, BottomText.c_str());
 	BottomText = "";
 	
 	refresh();
-	prefresh(TopPad, 0, ScrollX,0,0,1,Width-1);
 	
-	touchwin(Pad);
-	/*for (int i = 0; i < (int)PrinterList.size(); i++)
-	{
-		//prefresh(PrinterList[i].Pad, 0, 0, i*PrinterHeight+1, 0, PrinterHeight, PrinterWidth);
-		
-		//wrefresh(PrinterList[i].Pad);
-	}*/
-	prefresh(Pad, ScrollY, ScrollX,1,0,Height-2,Width-1);
+	//touchwin(Pad);
+	//prefresh(Pad, ScrollY, ScrollX, 1, 0, Height-2, Width-1);
+	prefresh(Pad, ScrollY, ScrollX, 1, 0, Height-2, Width-1);
+	
+	prefresh(TopPad, 0, ScrollX,0,0,1,Width-1);
 }
